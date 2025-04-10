fdc82071428977ab854a48458d9a56fa74238f26
Luc Berger <luc.richard1994@gmail.com>
2025-04-10 23:21:25 +0100

    feat: add patient details to a basic patient view

--- src/app/patient/[patientId]/page.tsx
+++ src/app/patient/[patientId]/page.tsx
@@ -1,4 +1,5 @@
 import { HydrateClient, api } from "@lutra/trpc/server";
+import { formatDate } from "@lutra/utils/date";
 
 export default async function PatientView({
 	params,
@@ -11,11 +12,26 @@ export default async function PatientView({
 			<div className="space-y-4 p-4 sm:p-6 lg:p-8">
 				<header>
 					<div className="flex items-center justify-between">
-						<h3 className="font-semibold text-lg">
-							Patient: {patient?.firstName} {patient?.lastName}
-						</h3>
+						<h3 className="font-semibold text-lg">Patient Details</h3>
 					</div>
 				</header>
+				<main>
+					{patient && (
+						<>
+							<p className="text-gray-600">First name: {patient.firstName}</p>
+							<p className="text-gray-600">Last name: {patient.lastName}</p>
+							<p className="text-gray-600">
+								Date of birth: {patient.dateOfBirth}
+							</p>
+							<p className="text-gray-600">Email: {patient.email}</p>
+							{patient.updatedAt && (
+								<p className="text-gray-600">
+									Last updated: {formatDate(patient.updatedAt)}
+								</p>
+							)}
+						</>
+					)}
+				</main>
 			</div>
 		</HydrateClient>
 	);
