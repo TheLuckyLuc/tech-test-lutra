afb5fcaa2ff3fc480c8ea97f98d30fc346d18198
Luc Berger <luc.richard1994@gmail.com>
2025-04-11 08:37:51 +0100

    refactor: replace error classes with TRPCError

--- src/server/api/routers/appointment/repository/appointment.repository.ts
+++ src/server/api/routers/appointment/repository/appointment.repository.ts
@@ -1,44 +1,35 @@
-import { eq } from "drizzle-orm";
+import { eq } from 'drizzle-orm';
+import { TRPCError } from '@trpc/server';
 
-import { db } from "@lutra/server/db";
-import { appointments } from "@lutra/server/db/schema";
+import { db } from '@lutra/server/db';
+import { appointments } from '@lutra/server/db/schema';
 
-export type CreateAppointmentParams = Omit<
-	typeof appointments.$inferInsert,
-	"id"
->;
+export type CreateAppointmentParams = Omit<typeof appointments.$inferInsert, 'id'>;
 
 class AppointmentRepository {
-	async createAppointment(appointmentOptions: CreateAppointmentParams) {
-		try {
-			await db.insert(appointments).values(appointmentOptions);
-		} catch (err) {
-			console.error(
-				`Error creating appointment for patient ID "${appointmentOptions.patientId}":`,
-				err,
-			);
-			throw new Error(
-				`Failed to create an appointment for patient with ID "${appointmentOptions.patientId}"`,
-			);
-		}
-	}
+  async createAppointment(appointmentOptions: CreateAppointmentParams) {
+    try {
+      await db.insert(appointments).values(appointmentOptions);
+    } catch (err) {
+      console.error(`Error creating appointment for patient ID "${appointmentOptions.patientId}":`, err);
+      throw new TRPCError({
+        code: 'INTERNAL_SERVER_ERROR',
+        message: `Failed to create appointment for patient ID "${appointmentOptions.patientId}"`,
+      });
+    }
+  }
 
-	async getAppointmentsByPatientId(patientId: number) {
-		try {
-			return await db
-				.select()
-				.from(appointments)
-				.where(eq(appointments.patientId, patientId));
-		} catch (err) {
-			console.error(
-				`Error fetching appointments for patient ID "${patientId}": `,
-				err,
-			);
-			throw new Error(
-				`Failed to fetch appointments for patient with ID "${patientId}"`,
-			);
-		}
-	}
+  async getAppointmentsByPatientId(patientId: number) {
+    try {
+      return await db.select().from(appointments).where(eq(appointments.patientId, patientId));
+    } catch (err) {
+      console.error(`Error fetching appointments for patient ID "${patientId}": `, err);
+      throw new TRPCError({
+        code: 'INTERNAL_SERVER_ERROR',
+        message: `Failed to fetch appointments for patient ID "${patientId}"`,
+      });
+    }
+  }
 }
 
 export const appointmentRepository = new AppointmentRepository();
--- src/server/api/routers/appointment/service/appointment.service.ts
+++ src/server/api/routers/appointment/service/appointment.service.ts
@@ -1,29 +1,32 @@
-import {
-	type CreateAppointmentParams,
-	appointmentRepository,
-} from "../repository/appointment.repository";
+import { TRPCError } from '@trpc/server';
+
+import { type CreateAppointmentParams, appointmentRepository } from '../repository/appointment.repository';
 
 class AppointmentService {
-	async getAppointmentsByPatientId(input: string) {
-		const id = Number(input);
+  async getAppointmentsByPatientId(input: string) {
+    const id = Number(input);
 
-		if (Number.isNaN(id)) {
-			throw new Error(`Invalid ID: ${input}. ID must be an integer.`);
-		}
+    if (Number.isNaN(id)) {
+      throw new TRPCError({
+        code: 'BAD_REQUEST',
+        message: `Invalid ID: ${input}. ID must be an integer.`,
+      });
+    }
 
-		return await appointmentRepository.getAppointmentsByPatientId(id);
-	}
+    return await appointmentRepository.getAppointmentsByPatientId(id);
+  }
 
-	async createAppointment(options: CreateAppointmentParams) {
-		try {
-			await appointmentRepository.createAppointment(options);
-		} catch (error) {
-			console.error("Error creating appointment: ", error);
-			throw new Error(
-				`Failed to create appointment for patient ID "${options.patientId}"`,
-			);
-		}
-	}
+  async createAppointment(options: CreateAppointmentParams) {
+    try {
+      await appointmentRepository.createAppointment(options);
+    } catch (error) {
+      console.error('Error creating appointment: ', error);
+      throw new TRPCError({
+        code: 'INTERNAL_SERVER_ERROR',
+        message: `Failed to create appointment for patient ID "${options.patientId}"`,
+      });
+    }
+  }
 }
 
 export const appointmentService = new AppointmentService();
